e3b1e88c6c3ed08b9e2977358ea92ca83ebb69ec
Nicole Maria da Silva Candido <Nicolecandido10@gmail.com>
2020-05-04 16:00:27 -0300

    Add files via upload

---
+++ Semana4/ListaAdicional/ex2.py
@@ -0,0 +1,15 @@
+def mult(X, Y):
+    return X * Y
+
+def soma(X, Y):
+    return X + Y
+    
+
+som = 0
+N1= int(input("Insira o primeiro valor: "))
+N2= int(input("Insira o segundo Valor: "))
+
+M = (soma(N1, N2))
+R = (mult(M, 2))
+
+print(R)
